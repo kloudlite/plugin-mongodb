11dfacfcdb1d9fd39c0b4f5f58b77f9d740cc13b
nxtcoder17 <nxtcoder17@gmail.com>
2024-12-16 18:42:32 +0530

    init: plugin mongodb

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "development workspace";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          # hardeningDisable = [ "all" ];
+
+          buildInputs = with pkgs; [
+
+            (stdenv.mkDerivation rec {
+              name = "run";
+              pname = "run";
+              src = fetchurl {
+                url = "https://github.com/nxtcoder17/Runfile/releases/download/v1.1.2/run-linux-amd64";
+                sha256 = "sha256-SKLuX/i86cpryK1TFOrtXGLikikwEuH0dPU4xur6N/Y=";
+              };
+              unpackPhase = ":";
+              installPhase = ''
+                mkdir -p $out/bin
+                cp $src $out/bin/$name
+                chmod +x $out/bin/$name
+              '';
+            })
+
+            # version control
+            git
+
+            # programming tools
+            go
+            kubebuilder
+          ];
+
+          shellHook = ''
+          '';
+        };
+      }
+    );
+}
